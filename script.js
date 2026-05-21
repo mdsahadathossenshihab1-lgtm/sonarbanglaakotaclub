471876c34fe51394916b7e93aca017a8e8f6d3ef
shihab <mdsahadathossenshihab1@gmail.com>
2026-05-21 21:21:55 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // আপনার গুগল অ্যাপস স্ক্রিপ্ট থেকে পাওয়া Web App URL-টি এখানে বসাবেন
-const scriptURL = 'https://script.google.com/macros/s/AKfycbzs6n8rf0acEYWxIMGoHByRbmbdkGbcPzm7n8aqljr3oeJ1uYPfq7Ald1JOxoeCXXJu_A/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw52A3KWKtYlWJZ92j51unj5wi1011ku_gNzJUGMKoOOrRP1omadWRqZiDMJmK8K_d-Dw/exec';
 
 const form = document.getElementById('advancedRegistrationForm');
 const submitBtn = document.getElementById('submitBtn');
